702151512788c5e1b0481505325436393b79b28e
Webdev2024 <Apoorvagowda551@gmail.com>
2023-07-01 14:29:39 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { User } from './user';
+import { RegistrationService } from './registration.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
+  topics=['Angular','python','Devops'];
+  topicHasError=true;
+  userModel= new User('rob','rob@gmail.com',78267434,'default','morning',true);
+
+  constructor(private  _registrationService:RegistrationService){}
+
+  validateTopic(value :any) {
+    if(value == 'default'){
+      this.topicHasError=true;
+    }
+    else{
+      this.topicHasError=false;
+    }
+
+  }
+  onSubmit(){
+    this._registrationService.enroll(this.userModel)
+    .subscribe(
+      data=> console.log('Success', data),
+      error=>console.error('Error',error)
+
+    )
+  }
 }
